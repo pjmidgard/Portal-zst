35da811ee9e6032dbfa04066a40fc4f761d12264
Java Software <pj.midgard@yahoo.com>
2021-08-13 10:10:05 +0100

    Update Portal7.py

--- Portal7.py
+++ Portal7.py
@@ -8,7 +8,7 @@ namez = input("ul or for compress cl for extract for compress zst cld fo extract
 class compression:
     def cryptograpy_compression(self):
                
-                self.name = "Written: Jurijus pacalovas Price Protal 5 000 000 Euro cost Date: 13/08/2021 9:23 Deep 14.5 ERA"
+                self.name = "Written: Jurijus pacalovas Price Protal 5 000 000 Euro cost Date: 13/08/2021 10:09 Deep 14.5 ERA"
                 if namez=="ul":
                     corridors=0
                     cor=7
